dee4062c4d2b730fb5da83fe205bcfc41d6ca897
Alam Guardin <alamguardin@gmail.com>
2025-01-24 22:51:36 -0500

    feat: add and remove alert

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello Alert");
+class Alert extends HTMLElement {
+	constructor() {
+		super();
+		this.richColor = false;
+	}
+
+	#removeAlert() {
+		setTimeout(() => {
+			this.lastChild.remove();
+		}, 2000);
+	}
+
+	default(message) {
+		this.innerHTML += `<div>${message}</div>`;
+		this.#removeAlert();
+	}
+}
+
+customElements.define("use-alert", Alert);
+
+const alert = document.querySelector("#alert");
+
+document.addEventListener("click", () => alert.default("This is my Alert"));
